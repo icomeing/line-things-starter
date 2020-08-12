58b0e89908e779f19650a1f812c6523e8f330ed8
icomeing <milkjac@gmail.com>
2020-08-12 09:31:50 +0800

    Update liff.js

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '4efafb52-05b9-4252-9634-41d782476a37'; // LED, Button
+const USER_SERVICE_UUID         = 'f5184d1e-4a92-4bc8-b046-1395df6bab16'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
