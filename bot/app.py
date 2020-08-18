29e8f0d0eb02ac4ddd0ba6900add7583c73e0849
icomeing <milkjac@gmail.com>
2020-08-18 09:58:00 +0800

    Update app.py

--- bot/app.py
+++ bot/app.py
@@ -53,7 +53,7 @@ def handle_things_event(event):
     humidity_byte= payload_byte[2];
     temperature = int.from_bytes(temperature_byte, 'little')/100.00;
     humidity= int.from_bytes(humidity_byte, 'little');
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text="溫度:"+str(temperature)+",濕度:"+str(humidity)));
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text="T:"+str(temperature)+",H:"+str(humidity)));
 
 if __name__ == "__main__":
     app.run(debug=True)
